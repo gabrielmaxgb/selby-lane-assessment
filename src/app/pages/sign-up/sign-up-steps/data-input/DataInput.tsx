101d8ce7e9f3a9823c396ae7a99c79bd0de6b765
Gabriel Max <gmaxgomes@gmail.com>
2023-06-14 12:47:26 -0300

    improve routes

--- src/app/pages/sign-up/sign-up-steps/data-input/DataInput.tsx
+++ src/app/pages/sign-up/sign-up-steps/data-input/DataInput.tsx
@@ -1,9 +1,15 @@
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const DataInput = () => {
+  const navigate = useNavigate();
+
   return (
     <Grid item container xs={12} alignItems={'center'} justifyContent={'center'} height={'100%'}>
-      <button>DataInput</button>
+      you're at 2
+      <button onClick={() => navigate('/signup/3')}>
+        <Typography>3</Typography>
+      </button>
     </Grid>
   );
 };
--- src/app/pages/sign-up/sign-up-steps/steps-description/StepsDescription.tsx
+++ src/app/pages/sign-up/sign-up-steps/steps-description/StepsDescription.tsx
@@ -1,9 +1,15 @@
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const StepsDescription = () => {
+  const navigate = useNavigate();
+
   return (
     <Grid item container xs={12} alignItems={'center'} justifyContent={'center'} height={'100%'}>
-      StepsDescription
+      you're at 1
+      <button onClick={() => navigate('/signup/2')}>
+        <Typography>2</Typography>
+      </button>
     </Grid>
   );
 };
